401678a733d529c1a7a7cf933e3def9ab48e1680
lihaiyang <lihaiyang@zhongfu.net>
2021-05-07 10:05:31 +0800

    框架搭建

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'chat-wordcloud'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
